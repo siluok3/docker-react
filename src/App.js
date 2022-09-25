ecfd40ec30c3dbe49967e91de8ec2a9483ed195b
Kiriakos Papachristou <koulispapas@gmail.com>
2022-09-25 22:18:30 +0200

    fix failed test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Up and running
+          Up and running!
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+    
+        </a>
       </header>
     </div>
   );
